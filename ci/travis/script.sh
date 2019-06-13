e7453cd80a6b8fa3f54d0371a8164cf8eed52b75
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2019-06-13 06:18:50 -0700

    Make `Travis-CI` run test outside of tarpaulin before measuring coverage

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -2,7 +2,12 @@
 
 . $(dirname $0)/functions.sh
 
+# --- Test -------------------------------------------------------------------
+
+log Testing code in debug mode
+cargo test
+
 # --- Test with coverage -----------------------------------------------------
 
-log Measuring code coverage through Tarpaulin with default features
+log Measuring code coverage in debug mode
 cargo tarpaulin -v --out Xml --ciserver travis-ci
