d1349cac4bf3a6c47b6bef37c3d5c1558ef2d229
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2019-07-12 02:20:51 +0000

    Add top level `parse` function to parse an ontology document

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,17 @@ extern crate pest;
 pub mod error;
 pub mod from_pair;
 pub mod parser;
+
+use curie::PrefixMapping;
+use horned_owl::model::Ontology;
+use horned_owl::model::Build;
+
+use self::parser::OwlFunctionalParser;
+use self::parser::Rule;
+use self::from_pair::FromPair;
+use self::error::Error;
+
+pub fn parse(src: &str) -> Result<(Ontology, PrefixMapping), Error> {
+    let mut pair = OwlFunctionalParser::parse(Rule::OntologyDocument, src)?.next().unwrap();
+    FromPair::from_pair(pair, &Build::new(), &PrefixMapping::default())
+}
