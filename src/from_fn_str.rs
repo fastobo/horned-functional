28cad2458ff1d2f0f28a8da2067cd87eec74e5dd
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2019-07-15 23:56:39 +0000

    Make sure `FromFunctional::from_ofn` consumes its entire input

--- src/from_fn_str.rs
+++ src/from_fn_str.rs
@@ -1,13 +1,15 @@
 use curie::PrefixMapping;
 use horned_owl::model::Build;
 
+use crate::error::Error;
 use crate::error::Result;
 use crate::from_pair::FromPair;
 use crate::parser::OwlFunctionalParser;
 
 /// A trait for OWL elements that can be deserialized from OWL strings.
 ///
-/// This i
+/// The deserialization will fail if the entirety of the input string cannot
+/// be deserialized into the declared type.
 pub trait FromFunctional: Sized + FromPair {
     /// Deserialize a string containing an OWL element in functional syntax.
     fn from_ofn(s: &str, build: &Build, prefixes: &PrefixMapping) -> Result<Self>;
@@ -33,10 +35,54 @@ impl<T: Sized + FromPair> FromFunctional for T {
     fn from_ofn(s: &str, build: &Build, prefixes: &PrefixMapping) -> Result<Self> {
         for rule in T::RULES {
             if let Ok(mut pairs) = OwlFunctionalParser::parse(*rule, s) {
-                return T::from_pair(pairs.next().unwrap(), build, prefixes);
+                if pairs.as_str().len() == s.len() {
+                    return T::from_pair(pairs.next().unwrap(), build, prefixes);
+                } else {
+                    return Err(
+                        Error::from(
+                            pest::error::Error::new_from_span(
+                                pest::error::ErrorVariant::CustomError {
+                                    message: "remaining input".to_string(),
+                                },
+                                pest::Span::new(s, pairs.as_str().len(), s.len()).unwrap()
+                            )
+                        )
+                    )
+                }
             }
         }
 
-        panic!()
+        return Err(
+            Error::from(
+                pest::error::Error::new_from_span(
+                    pest::error::ErrorVariant::ParsingError {
+                        positives: Vec::new(),
+                        negatives: Self::RULES.iter().cloned().collect(),
+                    },
+                    pest::Span::new(s, 0, s.len()).unwrap()
+                )
+            )
+        );
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    use horned_owl::model::DeclareClass;
+    use super::*;
+
+    #[test]
+    fn test_remaining_input() {
+        match DeclareClass::from_ofn_str("Class(<http://example.com/a>) Class(<http://example.com/b>)") {
+            Ok(ok) => panic!("unexpected success: {:?}", ok),
+            Err(Error::PestError(e)) => {
+                assert_eq!(e.variant, pest::error::ErrorVariant::CustomError {
+                    message: "remaining input".to_string(),
+                })
+            }
+            Err(other) => panic!("unexpected error: {:?}", other)
+        }
     }
 }
